53df93d3398911181c03fdf12af6c807b31e7718
Farhaan <patel.farhaaan@gmail.com>
2021-07-11 14:04:41 -0400

    start.sh updated

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 source venv/bin/activate
 pip3 install -r requirements.txt
-gunicorn --workers=2 --threads=4 --bind=127.0.0.1:5000 app:app
+gunicorn --workers=2 --threads=4 --bind=0.0.0.0:5000 app:app
