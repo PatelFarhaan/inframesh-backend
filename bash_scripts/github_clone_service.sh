600d25b37ad2afa51fddfd2ad6c99a9170be49d2
zequbal <zafar.e@karza.in>
2021-07-15 13:07:18 +0530

    Add git clone sh file

---
+++ bash_scripts/github_clone_service.sh
@@ -0,0 +1,15 @@
+github_username=$github_username
+ssh_username=$ssh_username
+github_token=$github_token
+repo_name=$repo_name
+project_name=$project_name
+
+# ssh_username=$1
+# project_name=$2
+# github_username=$3
+# github_token=$4
+# repo_name=$5
+
+mkdir -p /home/$ssh_username/$project_name
+cd /home/$ssh_username/$project_name
+git clone https://$github_username:$github_token@github.com/$github_username/$repo_name.git
